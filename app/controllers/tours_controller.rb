8eda4f8af449049cf0a53a31e6149c3add2c86d7
Alan e Charles <alanoliveiran45@gmail.com>
2017-03-24 21:56:44 -0300

    feat: add action buttons to tours table
    Also moved admin actions from regular ToursController to Admin namespace
    and tests refactored

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,41 +1,5 @@
 class ToursController < ApplicationController
-  def new
-    @tour = Tour.new
-  end
-
-  def create
-    @tour = Tour.new(tour_params)
-    if @tour.save
-      redirect_to @tour
-    else
-      flash[:notice] = 'Passeio não pode ser cadastrado, verifique os dados'
-      render :new
-    end
-  end
-
   def show
     @tour = Tour.find(params[:id])
-  end
-
-  def edit
-    @tour = Tour.find(params[:id])
-  end
-
-  def update
-    @tour = Tour.find(params[:id])
-    if @tour.update(tour_params)
-      redirect_to @tour
-    else
-      flash[:notice] = 'Não foi possível atualizar o passeio'
-      render :edit
-    end
-  end
-
-  private
-
-  def tour_params
-    params.require(:tour).permit(:name, :description, :category_id, :attraction,
-                                  :duration, :adult_price, :child_price, :baby_price,
-                                  :distance,:site)
   end
 end
--- spec/features/user_edit_tour_spec.rb
+++ spec/features/user_edit_tour_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 feature 'User edit tour' do
-  
   scenario 'successfully' do
+    user = login
 
     tour = create(:tour)
     new_name = 'Bike Rio Sucuri'
 
-    visit edit_tour_path(tour)
+    visit edit_admin_tour_path(tour)
     fill_in 'Nome', with: new_name
-    click_on 'Atualizar Passeio'
+    click_on 'Salvar'
 
     expect(page).to have_css('h1', text: new_name)
-
   end
 
   scenario 'with empty field' do
+    user = login
 
     tour = create(:tour)
 
-    visit edit_tour_path(tour)
+    visit edit_admin_tour_path(tour)
     fill_in 'Nome', with: ''
-    click_on 'Atualizar Passeio'
+    click_on 'Salvar'
 
     expect(page).to have_content 'Não foi possível atualizar o passeio'
-
   end
-
 end
